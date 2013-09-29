3e42e744af86fcbcfc5eca574a3036f41efddbaa
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-29 11:11:40 +0200

    Initial implementation for installing SABnzbd

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,17 @@
-class sabnzbd {
-  anchor { 'Hello_World': }
+# Public: Install SABnzbd to /Applications
+#
+# Examples
+#
+#  include sabnzbd
+#  class { 'sabnzbd':
+#    version => '0.7.16'
+#  }
+#
+class sabnzbd($version='0.7.16') {
+
+  package { "SABnzbd-${version}":
+    provider => 'appdmg',
+    source   => "http://kent.dl.sourceforge.net/project/sabnzbdplus/sabnzbdplus/${version}/SABnzbd-${version}-osx.dmg",
+  }
+
 }
--- spec/classes/sabnzbd_spec.rb
+++ spec/classes/sabnzbd_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe 'sabnzbd' do
-  it do
-    should contain_anchor('Hello_World')
-  end
+
+  version = '0.7.16'
+
+  it { should contain_class('sabnzbd') }
+  it { should contain_package("SABnzbd-#{version}").with_provider('appdmg') }
+  it { should contain_package("SABnzbd-#{version}").with_source("http://kent.dl.sourceforge.net/project/sabnzbdplus/sabnzbdplus/#{version}/SABnzbd-#{version}-osx.dmg") }
+
 end
